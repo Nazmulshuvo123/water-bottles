d34762db80d8562d5934adf8964ccda4401017bb
Nazmulshuvo123 <nazmulshuvo161199@gmail.com>
2024-10-21 23:28:24 +0600

    Create Shopping Cart to handle Event with parameters

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,11 +1,13 @@
 import './Bottle.css'
-const Bottle = ({bottle}) => {
+const Bottle = ({bottle, handleAddToCart}) => {
     const{name, img, price} = bottle;
     return (
         <div className="bottle">
             <img src={img} alt="" />
             <h3>Name: {name}</h3>
             <h4>Price: $ {price}</h4>
+
+            <button onClick={ ()=> handleAddToCart(bottle)} >Purchase</button>
         </div>
     );
 };
